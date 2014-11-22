c33892616c2be24c78cc30bc7f2f78555c734842
Sun He <sunheehnus@gmail.com>
2014-11-22 14:07:00 +0800

    gas_station.java: update a more elegent way

--- gas_station.java
+++ gas_station.java
@@ -1,19 +1,19 @@
 public class Solution {
 	public int canCompleteCircuit(int[] gas, int[] cost) {
-		int [] delta = new int [gas.length << 1];
-		for (int i = 0; i < gas.length; i++)
-			delta [i + gas.length] = delta[i] = gas[i] - cost[i];
-		for (int length = 0, i = 0, gas_left = 0; i < gas.length << 1; i++) {
-			gas_left += delta[i];
-			length++;
-			while (gas_left < 0)
-			{
-				length--;
-				gas_left -= delta[i - length];
-			}
-			if (length == gas.length)
-				return (i - length + 1) % gas.length;
+        int pos = 0;
+        int preCost = 0;
+        int gasLeft = 0;
+		for (int i = 0; i < gas.length; i++) {
+			gasLeft += gas[i] - cost[i];
+            if (gasLeft < 0)
+            {
+                pos = i + 1;
+                preCost += gasLeft;
+                gasLeft = 0;
+            }
 		}
+        if (pos != gas.length && gasLeft + preCost >= 0)
+            return pos;
 		return -1;
 	}
 }
