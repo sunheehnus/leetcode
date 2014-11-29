4f128f547adfe4236dd24e84a8b6ecd88ca606e6
Sun He <sunheehnus@gmail.com>
2014-11-29 13:35:40 +0800

    renew populating_next_right_pointers_in_each_node.java

--- populating_next_right_pointers_in_each_node.java
+++ populating_next_right_pointers_in_each_node.java
@@ -7,32 +7,19 @@
  * }
  */
 public class Solution {
-	TreeLinkNode pre;
-	int get_depth(TreeLinkNode root) {
-		int depth = 0;
-		while (root != null) {
-			root = root.left;
-			depth++;
-		}
-		return depth;
-	}
-	void traverse_ID(TreeLinkNode node, int level, int ID) {
-		if (level < ID) {
-			traverse_ID(node.left, level + 1, ID);
-			traverse_ID(node.right, level + 1, ID);
-		}
-		else {
-			if (pre != null)
-				pre.next = node;
-			pre = node;
-		}
+	public void _connect(TreeLinkNode root) {
+        if (root != null) {
+            if (root.left != null)
+                root.left.next = root.right;
+            if (root.right != null)
+                root.right.next = root.next != null ? root.next.left : null;
+            _connect(root.left);
+            _connect(root.right);
+        }
 	}
 	public void connect(TreeLinkNode root) {
-		int depth = get_depth(root);
-		for (int i = 0; i < depth; i++) {
-			pre = null;
-			traverse_ID(root, 0, i);
-			pre.next = null;
-		}
-	}
+        if (root != null)
+            root.next = null;
+        _connect(root);
+    }
 }
