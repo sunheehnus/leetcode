b48b479764c48eb2caef357c6da46a0efc70b95b
Sun He <sunheehnus@gmail.com>
2014-11-30 10:47:43 +0800

    populating_nex~_ii: update

--- populating_next_right_pointers_in_each_node_ii.java
+++ populating_next_right_pointers_in_each_node_ii.java
@@ -7,33 +7,57 @@
  * }
  */
 public class Solution {
-	TreeLinkNode pre;
-	int get_depth(TreeLinkNode root) {
-		if (root == null)
-			return 0;
-		int l_length = get_depth(root.left);
-		int r_length = get_depth(root.right);
-		return (l_length > r_length ? l_length : r_length) + 1;
-	}
-	void traverse_ID(TreeLinkNode node, int level, int ID) {
-		if (level < ID) {
-			if (node.left != null)
-				traverse_ID(node.left, level + 1, ID);
-			if (node.right != null)
-				traverse_ID(node.right, level + 1, ID);
-		}
-		else {
-			if (pre != null)
-				pre.next = node;
-			pre = node;
-		}
-	}
-	public void connect(TreeLinkNode root) {
-		int depth = get_depth(root);
-		for (int i = 0; i < depth; i++) {
-			pre = null;
-			traverse_ID(root, 0, i);
-			pre.next = null;
-		}
-	}
+    class Node {
+        TreeLinkNode father, child;
+        Node(TreeLinkNode father, TreeLinkNode child) {
+            this.father = father;
+            this.child = child;
+        }
+    }
+    Node nextLevelStartPoint;
+    public void findNextLevelStartPoing(Node curPoint) {
+        if (nextLevelStartPoint == null) {
+            if (curPoint.child.left != null)
+                nextLevelStartPoint = new Node(curPoint.child, curPoint.child.left);
+            else if (curPoint.child.right != null)
+                nextLevelStartPoint = new Node(curPoint.child, curPoint.child.right);
+        }
+    }
+    public Node nextNode(TreeLinkNode father, TreeLinkNode child) {
+        if (child == father.left) {
+            if (father.right != null)
+                return new Node(father, father.right);
+        }
+        TreeLinkNode n = father.next;
+        while (n != null && n.left == null && n.right == null)
+            n = n.next;
+        if (n == null)
+            return null;
+        else if (n.left != null)
+            return new Node(n, n.left);
+        else
+            return new Node(n, n.right);
+    }
+    public void _connect() {
+        Node curPoint = nextLevelStartPoint;
+        Node nextPoint = nextNode(curPoint.father, curPoint.child);
+        nextLevelStartPoint = null;
+        while (nextPoint != null) {
+            findNextLevelStartPoing(curPoint);
+            curPoint.child.next = nextPoint.child;
+            curPoint = nextPoint;
+            nextPoint = nextNode(curPoint.father, curPoint.child);
+        }
+        curPoint.child.next = null;
+        findNextLevelStartPoing(curPoint);
+    }
+    public void connect(TreeLinkNode root) {
+        if (root == null)
+            return;
+        root.next = null;
+        findNextLevelStartPoing(new Node(null, root));
+        while (nextLevelStartPoint != null) {
+            _connect();
+        }
+    }
 }
